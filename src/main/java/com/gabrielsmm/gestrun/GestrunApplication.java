7563df4465326abe808129a745163a1d0ed53066
gabrielsmm <gabrielsmm251@gmail.com>
2025-09-16 18:36:07 -0300

    init: criação do projeto

---
+++ src/main/java/com/gabrielsmm/gestrun/GestrunApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielsmm.gestrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestrunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestrunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielsmm/gestrun/GestrunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielsmm.gestrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestrunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
